87c864f52f2d0dc3204cc21916fd2d39195f8741
Younes <younesesebbar089@gmail.com>
2024-11-01 15:37:47 +0100

    use hashrouter instead of browseRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import QuizMaker from "./QuizMaker.jsx";
 import QuizResults from "./QuizResults.jsx";
